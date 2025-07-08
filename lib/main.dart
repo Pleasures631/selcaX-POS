743d3e28eb850e107d006e025752d786ff396a85
rnldy <renaldycahyaa@gmail.com>
2025-07-08 22:13:59 +0700

    add font setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,21 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SelcaX());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SelcaX extends StatelessWidget {
+  const SelcaX({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom UI App',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Login')),
-        body: const CustomTextField(),
+      theme: ThemeData(
+        fontFamily: 'Inter'
       ),
-    );
-  }
-}
-
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: 300,
-            height: 50,
-            child: TextField(
-              decoration: InputDecoration(hintText: 'Input Email', border: OutlineInputBorder()),
-            ),
-          ),
-          SizedBox(height: 24),
-          Container(
-            width: 300,
-            height: 50,
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(hintText: 'Input Password', border: OutlineInputBorder()),
-            ),
-          ),
-          SizedBox(height: 30),
-          Body3(),
-        ],
-      ),
-    );
-  }
-}
-
-class Body3 extends StatelessWidget {
-  const Body3({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(fontSize: 20),
-    );
-    return Column(
-      children: [
-        ElevatedButton(
-          style: style,
-          onPressed: () {},
-          child: const Text('Submit'),
-        ),
-      ],
+      title: 'Selca X POS',
+      home: LoginScreen()
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,80 @@
-  Future<void> handleLogin() async {
-    print("helloworld");
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Tes Font Poppins SemiBold',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 24,
+            ),
+          ),
+        ),
+        body: const CustomTextField(),
+      );
   }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 300,
+            height: 50,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Input Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
+          Container(
+            width: 300,
+            height: 50,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Input Password',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          Body3(),
+        ],
+      ),
+    );
+  }
+}
+
+class Body3 extends StatelessWidget {
+  const Body3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(fontSize: 20),
+    );
+    return Column(
+      children: [
+        ElevatedButton(
+          style: style,
+          onPressed: () {},
+          child: const Text('Submit'),
+        ),
+      ],
+    );
+  }
+}
